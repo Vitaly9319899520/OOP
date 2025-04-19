5d33751e432c7cb7770b321950bad0a052fa318f
Vitaly <9319899520@mail.ru>
2025-04-19 11:04:40 +0300

    Update product.py

--- src/product.py
+++ src/product.py
@@ -1,5 +1,88 @@
-
 from src.baseproduct import BaseProduct
 from src.baseproduct import PrintMixin
 
 
+class Product(BaseProduct, PrintMixin):
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        # super().__init__()
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+        super().__init__()
+        if quantity > 0:
+            self.quantity = quantity
+        else:
+            raise ValueError("Товар с нулевым количеством не будет быть добавлен")
+
+    def __str__(self):
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        if type(other) is Product:
+            # print(type(other))
+            return (self.__price * self.quantity) + (other.__price * other.quantity)
+        raise TypeError
+
+    @classmethod
+    def new_product(cls, new_product: dict):
+        """Взвращает созданный объект класса Product из параметров товара в словаре"""
+        # name = new_product["name"]
+        # description = new_product["description"]
+        # price = new_product["price"]
+        # quantity = new_product["quantity"]
+        # return cls(name, description, price, quantity)
+        return cls(**new_product)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if float(new_price) == 0 or float(new_price) < 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
+
+
+class Category:
+    name: str
+    description: str
+    products: list
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.__products = products
+        Category.category_count += 1
+        Category.product_count += len(self.__products)
+
+    def __str__(self):
+        return f"{self.name}, Количество продуктов: {sum(product.quantity for product in self.__products)} шт."
+
+    def add_product(self, product: Product):
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.category_count += 1
+        else:
+            raise TypeError
+
+    @property
+    def products(self):
+        product_list = ""
+        for product in self.__products:
+            product_list += f"{str(product)}"
+        return product_list
+
+    @property
+    # для тестов
+    def product_in(self):
+        return self.__products
